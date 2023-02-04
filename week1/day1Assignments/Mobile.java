7e07e708299fc17622c1ccbad17fb84deb4418c9
PersisD <124454051+PersisD@users.noreply.github.com>
2023-02-04 23:08:16 +0530

    week1

---
+++ week1/day1Assignments/Mobile.java
@@ -0,0 +1,22 @@
+package week1.day1Assignments;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName = "iPhone";
+		char mobileLogo = '@';
+		short noOfMobilePiece = 50;
+		int modelNumber = 11100;
+		long mobileImeiNumber = 8456721354645L;
+		float mobilePrice = 85000.99f;
+		boolean isDamaged = false;
+		
+		System.out.println(mobileBrandName);
+		System.out.println(mobileLogo);
+		System.out.println(noOfMobilePiece);
+		System.out.println(modelNumber);
+		System.out.println(mobileImeiNumber);
+		System.out.println(mobilePrice);
+		System.out.println(isDamaged);
+	}
+
+}
---
+++ week1/day1Assignments/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1Assignments;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		short noOfMirrors = 2;
+		long chassisNumber = 46327884651278L;
+		boolean isPunctured = false;
+		String bikeName = "Hero Passion PRO";
+		double runningKM = 13455.788554;
+		
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+				
+	}
+
+}
